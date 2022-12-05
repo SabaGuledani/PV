a19d1773682b274b42fe818f90c9bc6b8209f2c8
saba_guledani <saba.guledani.1@btu.edu.ge>
2022-12-05 21:33:28 +0400

    first

---
+++ app/src/main/java/com/example/pagerview/Page1Fragment.kt
@@ -0,0 +1,20 @@
+package com.example.pagerview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Page1Fragment : Fragment(R.layout.fragment_page1) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+
+
+}
